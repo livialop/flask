5a1d1b1320d72c2f1002887dfb8a3cc8af6a80cd
livialop <livvialn7@gmail.com>
2025-05-22 23:39:08 -0300

    update app.py and templates files

--- Pratica-03/app.py
+++ Pratica-03/app.py
@@ -1,16 +1,12 @@
 from flask import Flask, render_template, request, redirect, make_response, url_for, session
-from cryptography.fernet import Fernet
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
 
 # Definindo o app do flask
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "Zor{YD-}R%J?Y1=3iB*b0^]`AcYF."
+app.config['SECRET_KEY'] = "Zor{YD-}R%J?Y1=3iB*b0^]`AcYF." # Senha gerada em um site 
 
-# Chave para criptografia
-chave = Fernet.generate_key()
-fernet = Fernet(chave)
-
-# PRODUTOS
+# PRODUTOS -> Decidi que o site vai vender celulares. Preço ótimo
 PRODUTOS = {
     'iPhone 7':1200, 
     'Motorola X':200,
@@ -25,7 +21,8 @@ PRODUTOS = {
 # Página Inicial
 @app.route('/')
 def index():
-    return render_template('index.html')
+    nome = request.cookies.get('nome', 'Usuário')
+    return render_template('index.html', nome=nome)
 
 # Página de Cadastro
 @app.route('/cadastro', methods=['GET', 'POST'])
@@ -41,10 +38,10 @@ def cadastro():
         if email in session['users']:
             redirect(url_for('login')) 
 
-        else:
-            # FALTA CRIPTOGRAFAR A SENHA
-            session['users'][email] = senha
-            session['user'] = email
+        
+        senha_cripto = generate_password_hash(senha)
+        session['users'][email] = senha_cripto
+        session['user'] = email
 
         response = make_response(
             redirect(url_for('produtos'))
@@ -54,7 +51,7 @@ def cadastro():
         response.set_cookie('nome', nome, expires=data_expiracao)
 
         return response
-            # COLOCAR A CRIPTOGRAFIA DE SENHA DO USUÁRIO 
+            
 
     elif request.method == 'GET':
         return render_template('cadastro.html')
@@ -62,27 +59,28 @@ def cadastro():
 # Página de Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
+    if 'user' not in session:
+        return redirect(url_for('cadastro'))
+
     if request.method == 'POST':
         email = request.form.get('email')
         senha_login = request.form.get('senha')
 
-        if 'users' not in session:
-            return redirect(url_for('cadastro'))
-        
-        if email in session['users']:
-            senha = session['users'][email]
-            if senha_login == senha:
+        senha_cripto = session['users'][email]
+
+        if check_password_hash(senha_cripto, senha_login):
                 session['user'] = email
-                return redirect(url_for('produtos')) 
+                data_expiracao = datetime.now() + timedelta(days=7)
+                response = make_response(redirect(url_for('produtos')))
+                response.set_cookie('email', email, expires=data_expiracao)
+                response.set_cookie('nome', session['users'].get('nome', 'Usuário'), expires=data_expiracao)
+                return response
+        else:
+            return redirect(url_for('login'))
 
     return render_template('login.html') # Se o usuário errar a senha, a página aparece novamente vazia.
 
-# Deslogar o usuário atual
-@app.route('/logout', methods=['POST'])
-def logout():
-    session.pop('user')
-    return redirect(url_for('index'))
-    # FALTA ADICIONAR UMA ROTA PARA O USUÁRIO DAR LOGOUT
 
 # Página de Produtos
 @app.route('/produtos', methods=['GET'])
@@ -92,16 +90,46 @@ def produtos():
 
 @app.route('/carrinho', methods=['POST'])
 def carrinho():
-    produtos_selecionados = {}
+
+    if 'user' not in session:
+        return redirect(url_for('cadastro'))
+    if 'carrinho' not in session:
+        session['carrinho'] = {}
+
+    # produtos_selecionados = {}
     for produto in request.form:
-        produtos_selecionados[produto] = PRODUTOS[produto]
-    print(produtos_selecionados)
-    return render_template('carrinho.html', produtos=produtos_selecionados)
+        if produto in PRODUTOS:
+            session['carrinho'][produto] = PRODUTOS[produto]
 
-# IDEIA DE LISTA  DE PRODUTOS -> Ser um checkbox com as opções e no final um submit para enviar as informações.
+    session.modified = True
+    return redirect(url_for('ver_carrinho'))
+
+
+@app.route('/ver_carrinho', methods=['GET'])
+def ver_carrinho():
+    if 'user' not in session:
+        return redirect(url_for('cadastro'))
+    
+    carrinho = session.get('carrinho', {})
+    valor_total = sum(carrinho.values())
+
+    return render_template('carrinho.html', carrinho=carrinho, valor_total=valor_total)
+
+
+@app.route('/remover_do_carrinho', methods=['POST'])
+def remover_do_carrinho():
+    if 'user' in session:
+        session.pop('carrinho')
+    return redirect(url_for('ver_carrinho'))
+
 # FALTA TIRAR O HTML5 DAS PÁGINAS E USAR O FILE base.html PARA FAZER O HTML BÁSICO DAS PÁGS
-
 # LEMBRAR DE COLOCAR OS REQUIREMENTS.TXT AO FINAL DA ATIVIDADE
+
+# Deslogar o usuário atual
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop('user')
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
