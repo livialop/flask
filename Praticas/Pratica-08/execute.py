d8e844b783253d3c6e054eebd3740fbefc8d7535
livialop <livvialn7@gmail.com>
2025-07-23 08:09:58 -0300

    pratica08 concluida

--- Praticas/Pratica-08/execute.py
+++ Praticas/Pratica-08/execute.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('banco.db')
 
 conn.execute("PRAGMA foreign_keys = ON")
-INSERT = "INSERT INTO festa(titulo, user_id) VALUES ('EU', 11)"
+INSERT = "INSERT INTO festa(nome, valor, user_id) VALUES ('Gangnam Style 3000', 1100.2, 2), ('Eu amo minha namorada', 100000, 3), ('Oiii namorada', 99999, 4), ('Top 10 festas', 10, 6), ('Nao', 27,7)"
 conn.execute(INSERT)
 conn.commit()
 
