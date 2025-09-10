82f1402e392767e0029dc9882ade825a0cef593e
livialop <livvialn7@gmail.com>
2025-09-10 09:50:48 -0300

    aula sobre sqlalchemy

---
+++ Estudos/sqlalchemy2/sqlalchemy_teste.py
@@ -0,0 +1,34 @@
+# Criar o ambiente
+
+# pip install sqlalchemy
+from sqlalchemy import create_engine, text
+
+# Parte SQLITE
+SQLITE = "sqlite:///database.db"
+engine = create_engine(SQLITE)
+# conexao = engine.connect()
+with engine.connect() as conn:
+    SQL = text("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL           
+        )
+    """)
+    conn.execute(SQL)
+    conn.commit()
+
+# ---
+# pip install mysqlclient
+MYSQL = 'mysql://root:@localhost/flask'
+engine = create_engine(MYSQL)
+with engine.connect() as conn:
+    SQL = text("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER AUTO_INCREMENT PRIMARY KEY,
+            nome VARCHAR(120) NOT NULL           
+        )
+    """)
+    conn.execute(SQL)
+    conn.commit()
+
+# Para isso, é necessário abrir o Workbench e criar o schema com o nome desejado.
