5bc2857f4e7e2616b6df262f388e787a61abf1d6
livialop <livvialn7@gmail.com>
2025-11-04 15:29:03 -0300

    Adicionando o blueprint dos livros a aplicação.

--- projetos/biblioteca/app.py
+++ projetos/biblioteca/app.py
@@ -6,6 +6,7 @@ import config
 app: Flask = Flask(__name__)
 app.register_blueprint(controllers.auth.auth_bp)
 app.register_blueprint(controllers.main.main_bp)
+app.register_blueprint(controllers.livro.livros_bp)
 
 config.config(app)
 
